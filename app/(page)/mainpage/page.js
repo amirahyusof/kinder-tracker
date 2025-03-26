3641c5682b0006a712d954b6543d76bcaf053842
amirah <mirez.data@gmail.com>
2025-03-26 20:52:00 +0800

    fetch children activities and filtering the data

--- app/(page)/mainpage/page.js
+++ app/(page)/mainpage/page.js
@@ -1,8 +1,8 @@
 "use client"
 
 import React, {useState, useEffect} from "react";
-import ActivityCard from "@/app/interface/activityCard-Undone";
-import AvatarChild from "@/app/interface/avatarChild";
+import ActivityCard from "@/app/interface/dashboard/activityCard-Undone";
+import AvatarChild from "@/app/interface/dashboard/avatarChild";
 
 export default function Dashboard(){
   const [childData, setChildData] = useState([]);
@@ -12,17 +12,27 @@ export default function Dashboard(){
   useEffect(() => {
     //get child profile from local storage
     const data = JSON.parse(localStorage.getItem('childrenData') || '[]');
+    console.log("Loaded Children Data:", data); // Debugging
     setChildData(data);
+
+    //get list of undone activity from local storage
+    const activities = JSON.parse(localStorage.getItem('activityData') || '[]');
+    const undoneActivities = activities.filter((data) => data.status === "undone");
+    console.log("Loaded Activity Data:", activities); // Debugging
+    setActivityUndoneData(undoneActivities);
+
+    setLoading(false);
   }, []);
 
-  // if(loading) {
-  //   return <div>Loading...</div>;
-  // }
+  if (loading) return <div className="text-center p-6">Loading...</div>;
 
   return (
-    <section className="bg-[#FFF9CA]">
+    <section className="bg-[#FFF9CA] mx-10">
       <AvatarChild childData={childData} />
-      <ActivityCard data={activityUndoneData} />
+      <ActivityCard 
+        activityData={activityUndoneData} 
+        childData={childData}
+      />
     </section>
   );
 };
