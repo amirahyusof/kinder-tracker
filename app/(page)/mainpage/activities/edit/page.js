7be6223bb7bb0996f68ea593edcf4fe49d1f4e12
amirah <mirez.data@gmail.com>
2025-04-07 12:23:22 +0800

    changes function name

--- app/(page)/mainpage/activities/edit/page.js
+++ app/(page)/mainpage/activities/edit/page.js
@@ -6,11 +6,10 @@ import ReactConfetti from 'react-confetti';
 import { PartyPopper } from 'lucide-react';
 import { routeDB } from '@/app/firebase/api/route';
 
-export default function CreateActivityPage() {
+export default function EditActivityPage() {
   const router = useRouter();
   const searchParams = useSearchParams();
   const childId = searchParams.get('childId');
-  const userId = searchParams.get('userId');
   const activityId = searchParams.get('activityId');
   const { getActivityById, updateActivity } = routeDB();
   const [updatedActivity, setUpdatedActivity] = useState({
@@ -132,55 +131,60 @@ export default function CreateActivityPage() {
 
         </div>
       )}
-      <h1 className="text-xl mb-4">Edit Activity</h1>
-      <form onSubmit={handleSubmit} className="card-body">
-        <div className="form-control">
-          <label htmlFor="taskName" className="label">
-            <span className="text-black">Activity</span>
+
+      <h1 className="text-xl font-semibold text-gray-600 mb-4"> ✍️ Edit Activity</h1>
+      <form onSubmit={handleSubmit} className="w-full p-4">
+        {/* Edit activity */}
+        <div className="form-control mb-4">
+          <label htmlFor="activity" className="label">
+            <span className="block text-gray-700 font-medium">Activity</span>
           </label>
           <input
-            id="taskName"
+            id="activity"
             type="text"
-            className="input input-bordered input-md bg-white w-full max-w-xs"
+            className="input-sm flex-1 border-none bg-gray-100 px-3"
             value={updatedActivity.name || ''}
             onChange={(e) => setUpdatedActivity({...updatedActivity, name: e.target.value})}
             required
           />
         </div>
 
-        <div className="form-control">
-          <label htmlFor="taskDescription" className="label">
-            <span className="text-black">Description</span>
+        {/* edit avtivity description */}
+        <div className="form-control mb-4">
+          <label htmlFor="activityDescription" className="label">
+            <span className="block text-gray-700 font-medium">Description</span>
           </label>
           <textarea
-            id="taskDescription"
+            id="activityDescription"
             type="text"
-            className="input input-bordered input-md bg-white w-full max-w-xs"
+            className="input-md border-none bg-gray-100 p-3 rounded-lg"
             value={updatedActivity.description || ''}
             onChange={(e) => setUpdatedActivity({...updatedActivity, description:e.target.value})}
             required
           />
         </div>
 
-        <div className="form-control">
+        {/* edit due date */}
+        <div className="form-control mb-4">
           <label htmlFor="date" className="label">
-            <span className='text-black'>Due Date</span>
+            <span className='block text-gray-700 font-medium'>Date</span>
           </label>
           <input
             id="duedate"
             type="date"
-            className="w-full p-2 border rounded"
+            className="input-sm flex-1 border-none bg-gray-100 px-3"
             value={updatedActivity.dueDate || ''}
             onChange={(e) => setUpdatedActivity({...updatedActivity, dueDate:e.target.value})}
           />
         </div>
 
-        <div className="form-control">
-          <label className="block mb-2 text-black">Status</label>
+        {/* Edit status */}
+        <div className="form-control mb-4">
+          <label className="block mb-2 text-grqy-700 font-medium">Status</label>
           <select
             value={updatedActivity.status || ''}
             onChange={(e) => setUpdatedActivity({...updatedActivity, status: e.target.value})}
-            className="w-full p-2 border rounded"
+            className="w-full p-2 border-none bg-gray-100 px-3 rounded"
           >
             <option value="undone">Undone</option>
             <option value="in-progress">In Progress</option>
@@ -188,18 +192,20 @@ export default function CreateActivityPage() {
           </select>
         </div>
 
-        <div className="mt-6 space-y-2">
+        {/* Button */}
+        <div className="flex justify-end mt-6 space-x-2 md:space-x-4">
           <button
             type="submit"
-            className={`btn w-full bg-green-300 ${isEditing ? 'Editing' : ''}`}
+            className={`btn rounded-3xl border-green w-[100px] bg-green-400 text-white hover:bg-green-600
+               ${isEditing ? 'bg-green-400 cursor-not-allowed' : 'hover:bg-green-400'}`}
             disabled={isEditing}
           >
-            {isEditing ? 'Editing...' : 'Save Edit'}
+            {isEditing ? 'Editing...' : 'Save Editing'}
           </button>
 
           <button 
             type="button" 
-            className='btn btn-md btn-neutral w-full'
+            className='btn rounded-3xl border-white bg-red-600 hover:bg-red-400 text-white w-[100px]'
             onClick={() => handleCancel()}
           >
             Cancel
