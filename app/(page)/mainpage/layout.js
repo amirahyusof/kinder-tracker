aa071759b020b6b41c0a2ff4d27857471e5faa36
amirah <mirez.data@gmail.com>
2025-04-16 12:39:16 +0800

    remove unused component

--- app/(page)/mainpage/layout.js
+++ app/(page)/mainpage/layout.js
@@ -1,7 +1,5 @@
 import Layout from "@/app/interface/layout";
 import React from "react";
-import { ThemeContext } from "@/app/context/ThemeContext";
-
 
 export default function MainLayout({children}){
   return (
