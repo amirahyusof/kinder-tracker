7b06d3122cd9481dc58fdd10ffe053ceab925910
amirah <mirez.data@gmail.com>
2025-04-19 13:44:40 +0800

    modify layout

--- app/error.js
+++ app/error.js
@@ -8,7 +8,7 @@ export default function Error({ error, reset }) {
   }, [error]);
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-red-100 dark:bg-red-900 text-red-800 dark:text-red-100">
+    <div className="flex flex-col items-center w-full justify-center h-screen bg-red-100 dark:bg-red-900 text-red-800 dark:text-red-100">
       <h2 className="text-xl font-bold">Something went wrong!</h2>
       <p className="mt-2">{error?.message}</p>
       <button
